17668798db4dcdf63946c92e91b55e40a4deec30
Fajar-islami <ahmadfajarislami@protonmail.com>
2020-09-04 21:24:12 +0700

    Menambahkan isi detail blog

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { RegisterBg } from "../../assets";
+import "./detailBlog.scss";
 
 const DetailBlog = () => {
 	return (
-		<div>
-			<p>Detail Blog Page</p>
+		<div className="detail-blog-wrapper">
+			<img src={RegisterBg} alt="thumb" className="img-cover" />
+			<p className="blog-title">Title Blog</p>
+			<p className="blog-author">Author - Date Post</p>
+			<p className="blog-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas adipisci tenetur quis praesentium commodi amet tempore hic, quidem, nostrum tempora facere, minus vitae eveniet sequi? Eveniet ad nobis consequatur. Sequi!</p>
 		</div>
 	);
 };
