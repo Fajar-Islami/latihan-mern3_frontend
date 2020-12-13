5d123fd9216fdc20ef412962ae0d57d17181ff10
Fajar-islami <ahmadfajarislami@protonmail.com>
2020-12-13 14:48:43 +0700

    Video 43 || upload

--- src/components/molecules/BlogItem/index.js
+++ src/components/molecules/BlogItem/index.js
@@ -1,23 +1,26 @@
-import React from "react";
-import { RegisterBg } from "../../../assets";
-import "./blogItem.scss";
-import { Button, Gap } from "../../atoms";
-import { useHistory } from "react-router-dom";
+import React from 'react';
+import { RegisterBg } from '../../../assets';
+import './blogItem.scss';
+import { Button, Gap } from '../../atoms';
+import { useHistory } from 'react-router-dom';
 
-const BlogItem = () => {
-	const history = useHistory();
-	return (
-		<div className="blog-item">
-			<img src={RegisterBg} alt="post" className="image-thumb" />
-			<div className="content-detail">
-				<p className="title">Title Blog</p>
-				<p className="author">Author - Date Post</p>
-				<p className="body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi tenetur quae ipsum perferendis totam repudiandae, explicabo eligendi laborum quidem eaque officia beatae quibusdam animi asperiores? At debitis beatae repellat et?</p>
-				<Gap height={20} />
-				<Button title="View Detail" onClick={() => history.push("/detail-blog")} />
-			</div>
-		</div>
-	);
+const BlogItem = (props) => {
+  const history = useHistory();
+  const { image, title, name, date, body } = props;
+  return (
+    <div className='blog-item'>
+      <img src={image} alt='post' className='image-thumb' />
+      <div className='content-detail'>
+        <p className='title'> {title} </p>
+        <p className='author'>
+          {name} - {date}
+        </p>
+        <p className='body'>{body}</p>
+        <Gap height={20} />
+        <Button title='View Detail' onClick={() => history.push('/detail-blog')} />
+      </div>
+    </div>
+  );
 };
 
 export default BlogItem;
